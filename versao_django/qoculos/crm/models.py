d7b469a9412d45e4bdf8636d1586107009c854fb
JeanRabelo <jeanpablosousarabelo@gmail.com>
2025-07-24 00:02:44 -0300

    fazendo uma pagina mínima

--- versao_django/qoculos/crm/models.py
+++ versao_django/qoculos/crm/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class BlingAuth(models.Model):
+    """
+    Single row table storing oauth tokens.
+    """
+    # add primary key that is an integer
+    number = models.IntegerField(primary_key=True, default=1)
+    access_token = models.TextField()
+    refresh_token = models.TextField()
+    expires_at = models.DateTimeField()
+    bling_client_id = models.TextField()
+    bling_client_secret = models.TextField()
+    bling_redirect_uri = models.TextField()
+    bling_oauth_authorize = models.TextField()
+    bling_token_endpoint = models.TextField()
+
+    @property
+    def is_expired(self):
+        return timezone.now() >= self.expires_at
